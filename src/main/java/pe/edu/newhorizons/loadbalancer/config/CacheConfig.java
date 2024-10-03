b16dbf63d266bbc18c91f4e1bce47bbac4088da2
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-10-02 21:13:43 -0500

    LoadBalancer iter2

---
+++ src/main/java/pe/edu/newhorizons/loadbalancer/config/CacheConfig.java
@@ -0,0 +1,30 @@
+package pe.edu.newhorizons.loadbalancer.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CaffeineCacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("clientes");
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    @Bean
+    public Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterAccess(5, TimeUnit.MINUTES)
+                .weakKeys()
+                .recordStats();
+    }
+}
--- src/main/java/pe/edu/newhorizons/loadbalancer/routes/Routes.java
+++ src/main/java/pe/edu/newhorizons/loadbalancer/routes/Routes.java
@@ -1,6 +1,5 @@
 package pe.edu.newhorizons.loadbalancer.routes;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
